093312d8911131b224d173880448a25818e1d81a
Arun Kothari <58799086+arunkothari84@users.noreply.github.com>
2024-03-25 22:34:05 -0700

    Create 006.sql

---
+++ 006.sql
@@ -0,0 +1,10 @@
+-- Show first name and last name concatinated into one column to show their full name
+
+SELECT
+  CONCAT(first_name, ' ', last_name) AS full_name
+FROM patients;
+
+-- or
+
+-- SELECT first_name || ' ' || last_name
+-- FROM patients;
