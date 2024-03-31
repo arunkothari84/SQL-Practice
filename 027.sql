33b1e16e8b1d357c987adbdfceb71226e24c4a10
Arun Kothari <58799086+arunkothari84@users.noreply.github.com>
2024-03-30 20:59:28 -0700

    Create 027.sql

---
+++ 027.sql
@@ -0,0 +1,39 @@
+
+-- Show all patient's first_name, last_name, and birth_date who were born in the 1970s decade. Sort the list starting from the earliest birth_date
+
+select
+  first_name,
+  last_name,
+  birth_date
+from patients
+where
+  year(birth_date) between 1970 and 1979
+order by birth_date
+
+-- or
+-- SELECT
+--   first_name,
+--   last_name,
+--   birth_date
+-- FROM patients
+-- WHERE
+--   YEAR(birth_date) BETWEEN 1970 AND 1979
+-- ORDER BY birth_date ASC;
+-- or
+-- SELECT
+--   first_name,
+--   last_name,
+--   birth_date
+-- FROM patients
+-- WHERE
+--   birth_date >= '1970-01-01'
+--   AND birth_date < '1980-01-01'
+-- ORDER BY birth_date ASC
+-- or
+-- SELECT
+--   first_name,
+--   last_name,
+--   birth_date
+-- FROM patients
+-- WHERE year(birth_date) LIKE '197%'
+-- ORDER BY birth_date ASC
