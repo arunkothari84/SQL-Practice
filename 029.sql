c9539e650f9dbb95943f67814be595b0589eace6
Arun Kothari <58799086+arunkothari84@users.noreply.github.com>
2024-03-30 21:07:14 -0700

    Create 029.sql

---
+++ 029.sql
@@ -0,0 +1,12 @@
+
+-- Show the province_id(s), sum of height; where the total sum of its patient's height is greater than or equal to 7,000.
+
+select
+  province_id,
+  sum(height)
+from patients
+group by province_id
+having sum(height) >= 7000
+
+  -- or
+-- select * from (select province_id, SUM(height) as sum_height FROM patients group by province_id) where sum_height >= 7000;
